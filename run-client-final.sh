d410f063603fc65de14ead8d8b11b25f9387ed50
Xenofon (Fontas) Fafoutis <xefa@dtu.dk>
2019-10-08 13:09:30 +0200

    final client configuration

---
+++ run-client-final.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# This is the configuration of the final run.
+# The seed value will be a non-zero value that will not be disclosed. 
+#
+
+SERVER=192.168.101.10
+PORT=5003
+SEED=0
+TOTAL=1000
+START=0
+DIFFICULTY=30000000
+REP_PROB_PERCENT=20
+DELAY_US=750000
+PRIO_LAMBDA=1.5
+
+/home/vagrant/os-challenge-common/client $SERVER $PORT $SEED $TOTAL $START $DIFFICULTY $REP_PROB_PERCENT $DELAY_US $PRIO_LAMBDA
