d4035dcc737951bf0563f0e62af07218d083236d
Xenofon (Fontas) Fafoutis <xefa@dtu.dk>
2019-10-09 13:01:49 +0000

    client milestone config

---
+++ run-client-milestone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# This is the configuration of the milestone run.
+#
+
+SERVER=192.168.101.10
+PORT=5003
+SEED=3435245
+TOTAL=100
+START=0
+DIFFICULTY=30000000
+REP_PROB_PERCENT=20
+DELAY_US=750000
+PRIO_LAMBDA=1.5
+
+/home/vagrant/os-challenge-common/client $SERVER $PORT $SEED $TOTAL $START $DIFFICULTY $REP_PROB_PERCENT $DELAY_US $PRIO_LAMBDA
