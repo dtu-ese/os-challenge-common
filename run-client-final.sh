4143c0b0050add05c541db2cd4a6107a07ba7cc8
Xenofon (Fontas) Fafoutis <xefa@dtu.dk>
2022-05-20 08:31:04 +0000

    reduced delay

--- run-client-final.sh
+++ run-client-final.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # This is the configuration of the final run.
-# The seed value will be a non-zero value that will not be disclosed. 
+# The seed value will be a non-zero value that will not be disclosed.
 #
 
 SERVER=192.168.101.10
@@ -11,7 +11,7 @@ TOTAL=1000
 START=0
 DIFFICULTY=30000000
 REP_PROB_PERCENT=20
-DELAY_US=750000
+DELAY_US=600000
 PRIO_LAMBDA=1.5
 
 /home/vagrant/os-challenge-common/client $SERVER $PORT $SEED $TOTAL $START $DIFFICULTY $REP_PROB_PERCENT $DELAY_US $PRIO_LAMBDA
