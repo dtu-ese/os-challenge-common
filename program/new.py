dbac2ec83b7ef4c5c1c1032ad3596df1884fd14c
Chad Lantz <s241783@dtu.dk>
2024-10-03 10:00:50 +0200

    deleted file

--- program/new.py
+++
@@ -1,5 +0,0 @@
-
-    uint8_t prio;
-    memcpy(&prio, buffer + 32 + 8, sizeof(prio)); // Copy from buffer into prio
-    prio = be64toh(prio);                         // Convert from big-prioian to host byte order
-    printf("prio: %llu\n", (unsigned long long)prio);
