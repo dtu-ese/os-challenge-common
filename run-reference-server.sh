f611724cc9b939236897c26e52e7839958a35d11
likoly <nikisalsoer@hotmail.com>
2024-11-14 11:23:43 +0100

    path change

--- run-reference-server.sh
+++ run-reference-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATHTOCOMMON=~/operating/byteforce/os-challenge-ByteForce/
+PATHTOCOMMON=~/operating/byteforce/os-challenge-ByteForce
 PORT=5003
 
 $PATHTOCOMMON/$(./get-bin-path.sh)/server $PORT
