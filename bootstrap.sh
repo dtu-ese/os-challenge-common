b089d518b0a509bd4aa7c69c8d4a2a56a106347b
Xenofon Fafoutis <xefa@dtu.dk>
2026-05-12 12:26:13 +0200

    binaries for arm64 with lonesha256 provided

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@ apt update
 apt upgrade -y
 
 apt-get install -y git
-apt-get install -y libssl-dev
 apt-get install -y build-essential
 
 echo "export PATH=/home/vagrant/os-challenge-common/:$PATH" >> /home/vagrant/.bashrc
